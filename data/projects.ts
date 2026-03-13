b293335747039aa95f706ccbf0a844b4d409a5f5
Eduard Hernández Ventós <eduardhernandezventos@gmail.com>
2026-03-13 19:27:58 +0100

    updated link project

--- data/projects.ts
+++ data/projects.ts
@@ -11,38 +11,43 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: 'Case Study | Ticketeame',
-    description: 'Liderazgo de diseño y estrategia UX/UI para una plataforma transaccional B2B2C escalable.',
-    logo: '/logos/ticketeame.svg',
-    link: 'https://www.figma.com/proto/t4ySjWQYuC7TMqxS4nztx1/Ticketeame?page-id=1822%3A11699&node-id=6679-341&p=f&viewport=-84%2C543%2C0.14&t=QpnxX15XfUxj7xpu-1&scaling=contain&content-scaling=fixed&starting-point-node-id=6679%3A341',
-    slug: 'Ticketeame',
+    title: "Case Study | Ticketeame",
+    description:
+      "Liderazgo de diseño y estrategia UX/UI para una plataforma transaccional B2B2C escalable.",
+    logo: "/logos/ticketeame.svg",
+    link: "https://www.figma.com/proto/t4ySjWQYuC7TMqxS4nztx1/Ticketeame?page-id=1822%3A11699&node-id=6679-341&p=f&viewport=-84%2C543%2C0.14&t=QpnxX15XfUxj7xpu-1&scaling=contain&content-scaling=fixed&starting-point-node-id=6679%3A341",
+    slug: "Ticketeame",
   },
   {
-    title: 'VSCode Portfolio',
-    description: 'Portfolio interactivo desarrollado con Next.js que simula el entorno de VS Code, destacando mi perfil técnico.',
-    logo: '/logos/vsc.svg',
-    link: 'https://github.com/trosdesuru/vscode-porfolio',
-    slug: 'vscode-portfolio',
+    title: "VSCode Portfolio",
+    description:
+      "Portfolio interactivo desarrollado con Next.js que simula el entorno de VS Code, destacando mi perfil técnico.",
+    logo: "/logos/vsc.svg",
+    link: "https://github.com/trosdesuru/vscode-porfolio",
+    slug: "vscode-portfolio",
   },
   {
-    title: 'SaaS Logístico | Valerdat',
-    description: 'Diseño de dashboards analíticos e interfaces para la gestión de datos complejos en un entorno SaaS B2B.',
-    logo: '/logos/valerdat.png',
-    link: '#',
-    slug: 'valerdat',
+    title: "SaaS Logístico | Valerdat",
+    description:
+      "Diseño de dashboards analíticos e interfaces para la gestión de datos complejos en un entorno SaaS B2B.",
+    logo: "/logos/valerdat.png",
+    link: "#",
+    slug: "valerdat",
   },
   {
-    title: 'Al Borde del Burnout',
-    description: '',
-    logo: '/logos/icon-burnout.png',
-    link: '#',
-    slug: 'albordedelburnout',
+    title: "Al Borde del Burnout",
+    description:
+      "Diseño y desarrollo de la plataforma web oficial para el podcast Al Borde del Burnout.",
+    logo: "/logos/icon-burnout.png",
+    link: "https://abdb.vercel.app/",
+    slug: "albordedelburnout",
   },
   {
-    title: 'Cities App',
-    description: 'App móvil nacida de una necesidad real: descubrir lugares auténticos. Diseño UX/UI centrado en conectar a viajeros con las experiencias de la gente local, lejos de las rutas turísticas convencionales.',
-    logo: '/logos/cities.png',
-    link: 'https://www.behance.net/gallery/159297987/cities-UX-UI',
-    slug: 'coolify-vscode-extension',
+    title: "Cities App",
+    description:
+      "Diseño UX/UI & Desarrollo, centrado en conectar a viajeros con las experiencias de la gente local.",
+    logo: "/logos/cities.png",
+    link: "https://www.behance.net/gallery/159297987/cities-UX-UI",
+    slug: "coolify-vscode-extension",
   },
 ];
