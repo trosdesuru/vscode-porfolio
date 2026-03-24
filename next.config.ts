7f866a4ecaeb2e9ea88cb97bc33ff952586627b6
Eduard Hernández Ventós <eduardhernandezventos@gmail.com>
2026-03-24 12:36:11 +0100

    added new config on nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,21 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { hostname: 'res.cloudinary.com', protocol: 'https' },
-      { hostname: 'avatars.githubusercontent.com', protocol: 'https' },
-      { hostname: 'imgur.com', protocol: 'https' },
-      { hostname: 'media2.dev.to', protocol: 'https' },
+      { hostname: "res.cloudinary.com", protocol: "https" },
+      { hostname: "avatars.githubusercontent.com", protocol: "https" },
+      { hostname: "imgur.com", protocol: "https" },
+      { hostname: "media2.dev.to", protocol: "https" },
     ],
+  },
+  async rewrites() {
+    return [
+      { source: "/pdf", destination: "/" },
+      { source: "/mail", destination: "/" },
+      { source: "/linkedin", destination: "/" },
+      { source: "/test", destination: "/" },
+    ];
   },
 };
 
